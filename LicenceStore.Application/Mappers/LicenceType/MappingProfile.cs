2cf12cc42f0a0e65a77f2fc26cc09db659adb991
Sana19988 <tesica84@gmail.com>
2024-01-16 23:11:31 +0100

    Add Mapping for LicenceType. Fix spelling mistake in Licence. - https://singimail.atlassian.net/browse/DEV-8

---
+++ LicenceStore.Application/Mappers/LicenceType/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using LicenceStore.Application.Common.Dto.LicenceType;
+
+namespace LicenceStore.Application.Mappers.LicenceType;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<CreateLicenceTypeDto, LicenceStore.Domain.Entities.LicenceType>().ReverseMap();
+        CreateMap<UpdateLicenceTypeDto, LicenceStore.Domain.Entities.LicenceType>().ReverseMap();
+        CreateMap<LicenceStore.Domain.Entities.LicenceType, LicenceTypeDetailsDto>().ConstructUsing(x => GetLicenceTypeDetails(x));
+    }
+
+    private static LicenceTypeDetailsDto GetLicenceTypeDetails(Domain.Entities.LicenceType type)
+    {
+        return new LicenceTypeDetailsDto(type.Name, type.Active);
+    }
+    
+    // TODO Paginacija i ovde 
+    
+}
