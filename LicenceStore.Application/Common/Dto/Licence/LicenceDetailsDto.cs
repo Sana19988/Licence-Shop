52f9203f2722bf4ab01cb8ec8f1e754211c44d12
Sana19988 <tesica84@gmail.com>
2024-01-16 12:39:09 +0100

    Add Mapping Profile for Licence. - https://singimail.atlassian.net/browse/DEV-4

--- LicenceStore.Application/Common/Dto/Licence/LicenceDetailsDto.cs
+++ LicenceStore.Application/Common/Dto/Licence/LicenceDetailsDto.cs
@@ -1,3 +1,3 @@
 ﻿namespace LicenceStore.Application.Common.Dto.Licence;
 
-public record LicenceDetailsDto(string Name, DateTime StartDate, DateTime EndDate, string Vendor, string Category, string Type, string? Owner, bool IsSold, double Price, string? Sold, string? Bought, string Img, string Description);
+public record LicenceDetailsDto(string Name, DateTime StartDate, DateTime EndDate, string Vendor, string Category, string Type, string? Owner, bool IsSold, bool IsBought, double Price, string Img, string Description);
