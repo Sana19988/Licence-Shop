76ee195c1465ec843d19f270e9676867f3aae1a4
Sana19988 <tesica84@gmail.com>
2024-01-17 18:19:59 +0100

    Add mapper for category. - https://singimail.atlassian.net/browse/DEV-12

---
+++ LicenceStore.Application/Mappers/Category/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using LicenceStore.Application.Common.Dto.Category;
+
+namespace LicenceStore.Application.Mappers.Category;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<CreateCategoryDto, LicenceStore.Domain.Entities.Category>().ReverseMap();
+        CreateMap<UpdateCategoryDto, LicenceStore.Domain.Entities.Category>().ReverseMap();
+        CreateMap<LicenceStore.Domain.Entities.Category, CategoryDetailsDto>().ConstructUsing(cat => GetCategoryDetails(cat));
+    }
+
+    private static CategoryDetailsDto GetCategoryDetails(Domain.Entities.Category category)
+    {
+        return new CategoryDetailsDto(category.Name, category.Active);
+    }
+    
+    // TODO Paginacija from previous
+}
